c1a5b541102317b7b4c2eced0bcd3bece8095a67
Nick Strydom <nstrydom@gmail.com>
2019-08-21 09:12:51 -0500

    Changed print method

--- microblog.py
+++ microblog.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env Python3
+#! /usr/bin/env python3
 
 import argparse
 
@@ -9,12 +9,25 @@ from sqlalchemy.orm import sessionmaker
 from db.tabledef import Entry
 
 
-db_engine = create_engine('sqlite:///blog.sqlite3')
+db_engine = create_engine('sqlite:////home/ghost/PycharmProjects/microblog/blog.sqlite3')
 
 class MicroBlog:
     def __init__(self):
         self.db_session = sessionmaker(bind=db_engine)
         self.session = self.db_session()
+
+    def print_last_entries(self, num_entries):
+        try:
+            results = \
+                self.session.query(Entry.timestamp, Entry.content).order_by(Entry.timestamp.desc()).limit(num_entries)
+
+            print('[*] Printing {} results'.format(results.count()))
+
+            for row in results:
+                print('[*] {:.19} -- {}'.format(str(row[0]), row[1]))
+
+        except Exception as ex:
+            print('[*] Error -> ' + str(ex))
 
     def submit_entry(self, content):
         try:
@@ -38,9 +51,14 @@ if __name__ == '__main__':
     micro_blog = MicroBlog()
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-m', '--message', type=str)
+    parser.add_argument('-m', '--message', type=str, help='message to submit to microblog')
+    parser.add_argument('-p', '--print', type=int, default=10, required=False, help='prints last n entries')
 
     args = parser.parse_args()
-    micro_blog.submit_entry(args.message)
+
+    if args.message is not None:
+        micro_blog.submit_entry(args.message)
+    elif args.print is not None or args.print > 0:
+        micro_blog.print_last_entries(args.print)
 
 
